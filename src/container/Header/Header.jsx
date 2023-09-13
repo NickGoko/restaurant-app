08a5229d92b8978c42b6fc8b374f8af10cb17b57
NickGoko <nickmainagoko@gmail.com>
2023-09-13 19:07:59 +0300

    change the layout of header

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,22 +1,41 @@
 import React from 'react';
-
 import { SubHeading } from '../../components';
 import { images } from '../../constants';
 import './Header.css';
 
-const Header = () => (
-  <div className="app__header app__wrapper section__padding" id="home">
-    <div className="app__wrapper_info">
-      <SubHeading title="Chase the new flavour" />
-      <h1 className="app__header-h1">The Key To Fine Dining</h1>
-      <p className="p__opensans" style={{ margin: '2rem 0' }}>Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus </p>
-      <button type="button" className="custom__button">Explore Menu</button>
-    </div>
+const Header = () => {
+  // Define a function to handle the "Explore Menu" button click
+  const handleExploreMenuClick = () => {
+    // You can add functionality here, such as scrolling to the menu section
+    // For example, you can use a library like 'react-scroll' to smoothly scroll to the menu
+    // For now, let's just display an alert when the button is clicked.
+    alert('Button Clicked! Add your custom functionality here.');
+  };
 
-    <div className="app__wrapper_img">
-      <img src={images.welcome} alt="header_img" />
+  return (
+    <div className="app__header app__wrapper section__padding" id="home">
+      <div className="app__wrapper_info">
+        <SubHeading title="Chase the new flavor" />
+        <h1 className="app__header-h1">The Key To Fine Dining</h1>
+        <p className="p__opensans" style={{ margin: '2rem 0' }}>
+  Discover exquisite flavors and delightful dining experiences. Our carefully crafted menu offers a wide range of delectable dishes to satisfy your cravings. Join us for a memorable culinary journey.
+</p>
+        {/* Use an onClick handler for the button */}
+        <button
+          type="button"
+          className="custom__button"
+          onClick={handleExploreMenuClick}
+        >
+          Explore Menu
+        </button>
+      </div>
+
+      <div className="app__wrapper_img">
+        {/* Use 'alt' text that describes the image */}
+        <img src={images.welcome} alt="Delicious Food" />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Header;
